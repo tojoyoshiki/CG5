ccce40aeed7125669390709589e7035a268c1e5f
tojoyoshiki <k023g0101@g.neec.ac.jp>
2025-06-19 12:47:26 +0900

    修正後再提出

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -104,9 +104,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	//頂点データの準備
 	VertexData vertices[] = {
-	    {0.0f,  0.5f,  0.0f, 1.0f},
-	    {0.5f,  -0.5f, 0.0f, 1.0f},
-	    {-0.5f, -0.5f, 0.0f, 1.0f},
+	    {-1.0f, -1.0f, 0.0f, 1.0f}, // 左下
+	    {-1.0f, 3.0f,  0.0f, 1.0f}, // 左上の外
+	    {3.0f,  -1.0f, 0.0f, 1.0f}, // 右下の外
 	};
 
 	// VertexResourceの生成
@@ -124,9 +124,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	//頂点インデックスデータの準備
 	uint16_t indices[] = {
-	    0,
-	    1,
-	    2,
+	    0, 1, 2, // 1枚目の三角形（左上 → 右上 → 右下）
+	    0, 2, 3  // 2枚目の三角形（左上 → 右下 → 左下）
 	};
 
 	//indexbufferの生成
