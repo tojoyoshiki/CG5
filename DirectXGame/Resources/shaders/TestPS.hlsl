485f0236846eb624a1992756d455946d12c02147
tojoyoshiki <k023g0101@g.neec.ac.jp>
2025-06-26 11:50:04 +0900

    00_09エラー

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -13,11 +13,13 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
     
     float32_t2 uv = input.texcoord;
-    float32_t2 textureColor = gTexture.Sample(gSampler, uv);
+    float32_t4 textureColor = gTexture.Sample(gSampler, uv);
+    
+    output.color = textureColor;
     
    // float32_t4 uv = input.texcoord; // ← 修正！
 
-    output.color = float4(uv.x, uv.y, 0.0f, 1.0f);
+   // output.color = float32_t4(uv.x, uv.y, 0.0f, 1.0f);
     
     return output;
 }
